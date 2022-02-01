af17d6907dfda76cb9927af391b0ae2fb114ce51
Arturo Mata <81439664+matarturo@users.noreply.github.com>
2022-02-01 16:24:07 -0400

    Update ipyes.sh

--- ipyes.sh
+++ ipyes.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+Script: ipyes.sh
+Autor: Arturo Mata <arturo.mata@gmail.com>
+Version: 1.0.0
+Repositorio: https://github.com/matarturo/
+Released under the GNU General Public License WITHOUT ANY WARRANTY.
+See LICENSE.TXT for details.
+
+# Script escrito en lenguaje Bash para detectar hosts dispinobles en un segmento de red
 
 star=`date +%s`
 echo "*********Buscando...**********"
